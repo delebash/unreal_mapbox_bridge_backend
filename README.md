d64e02f42cb6325b05c4e0d9ea716faaa00944b5
Daniel Elebash <delebash@users.noreply.github.com>
2023-03-17 11:43:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Backend Server fro Unreal Mapbox Bridge 
+
+#### Use real world heightmap data from Mapbox and automatically convert it to an Unreal heightmap image
+
+# Check out the demo running live [here](https://terrain.justgeektechs.com/)
+
+
+# Plugin version for Unreal Egine 5 <a href="https://github.com/delebash/UnrealMapboxBridgePlugin" target="_blank">UnrealMapboxBridge</a>
+
+Get Help here <a href="https://discord.gg/2WkHWNDf8q" target="_blank">Discord Server</a>
+
+For what is coming next check the **<a href="https://trello.com/b/7jXYlo13/unreal-mapbox-bridge" target="_blank">RoadMap</a>**
+
+Other free Unreal projects
+
+https://github.com/delebash/unreal_vault_organizer
+
---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const rescue = require('express-rescue')
+const cors = require('cors')
+const combineTiles = require('combine-tiles')
+const sharp = require('sharp');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express()
+const fs = require('fs')
+const {v4: uuidv4} = require('uuid');
+
+app.use(cors());
+const port = 3000
+
+// app.use(express.raw());
+
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+
+async function run() {
+
+
+    app.post('/', rescue(async (req, res, next) => {
+        let files = await req.body
+
+        try {
+            let tiles = []
+            let tile = {}
+            let guid = uuidv4();
+            let destCombined = path.resolve('./uploads/' + 'combined' + '_' + guid + '.png');
+            for (let file of files) {
+                tile = {}
+                let array = JSON.parse("[" + file.buffer + "]");
+                let guid = uuidv4();
+                let outputFilePath = path.resolve('./uploads/' + guid + +'_' + file.name)
+
+                await sharp(Buffer.from(array), {}).toFile(outputFilePath)
+
+                tile.x = file.x
+                tile.y = file.y
+                tile.file = outputFilePath
+                tiles.push(tile)
+            }
+
+            const size = 512
+            console.log(tiles)
+            await combineTiles(tiles, size, size, destCombined)
+
+            const imageBuffer = fs.readFileSync(
+                path.resolve(destCombined),
+            )
+
+            res.send(Buffer.from(imageBuffer))
+            res.end();
+
+            for (let tile of tiles) {
+                fs.unlink(tile.file, function (err) {
+                    if (err) throw err;
+                    // if no error, file has been deleted successfully
+                    console.log('File deleted!');
+                });
+            }
+
+            fs.unlink(destCombined, function (err) {
+                if (err) throw err;
+                // if no error, file has been deleted successfully
+                console.log('File deleted!');
+            })
+
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }))
+
+    app.use((err, req, res, next) => {
+        res.send('error')
+    })
+
+    const server = await app.listen(port);
+}
+
+run()
