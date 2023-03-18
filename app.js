fa74024a7fc5f48ac38ab87f7917aaa110514231
Daniel Elebash <delebash@users.noreply.github.com>
2023-03-18 13:30:45 -0400

    increase upload limit

--- app.js
+++ app.js
@@ -3,28 +3,22 @@ const rescue = require('express-rescue')
 const cors = require('cors')
 const combineTiles = require('./combine-tiles.js')
 const sharp = require('sharp');
-const bodyParser = require('body-parser');
-const path = require('path');
-
+const bodyParser = require('body-parser')
 
 const app = express()
-const fs = require('fs')
+
 const {v4: uuidv4} = require('uuid');
 
 app.use(cors());
 const port = 3000
 
-// app.use(express.raw());
-
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({limit: '1000mb'}));
+app.use(bodyParser.urlencoded({limit: '1000mb', extended: true}))
 
 async function run() {
 
     app.post('/backend', rescue(async (req, res, next) => {
         let tiles = await req.body
-
         try {
             for (let tile of tiles) {
                 tile.buffer = await sharp(Buffer.from(JSON.parse("[" + tile.buffer + "]")), {}).toBuffer()
